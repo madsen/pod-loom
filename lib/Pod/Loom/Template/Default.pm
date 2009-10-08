672ea0db9534e84b6c0b4b06d636d7c6308f5671
Christopher J. Madsen <perl@cjmweb.net>
2009-10-08 17:34:16 -0500

    Added documentation

--- lib/Pod/Loom/Template/Default.pm
+++ lib/Pod/Loom/Template/Default.pm
@@ -24,8 +24,25 @@ use Moose;
 extends 'Pod::Loom::Template';
 
 use Pod::Loom::Template '%E';
-
 #=====================================================================
+
+=attr sort_attr
+
+This should be 0, 1, or an arrayref.  If non 0, attributes will be
+sorted.  An arrayref lists the attributes that should come first, with
+the remaining attributes in alphabetical order.
+See L<Pod::Loom::Template/"Pod::Loom-sort_COMMAND">.
+
+=attr sort_diag
+
+Just like C<sort_attr>, but for diagnostic messages.
+
+=attr sort_method
+
+Just like C<sort_attr>, but for methods.
+
+=cut
+
 has qw(sort_attr   is ro), isa => 'Int | ArrayRef[Str]';
 has qw(sort_diag   is ro), isa => 'Int | ArrayRef[Str]';
 has qw(sort_method is ro), isa => 'Int | ArrayRef[Str]';
@@ -36,6 +53,7 @@ sub collect_commands
 } # end collect_commands
 
 #---------------------------------------------------------------------
+# Don't forget to update DESCRIPTION and sort_method if changing this:
 our @sections =
   (qw(NAME VERSION SYNOPSIS DESCRIPTION ATTRIBUTES METHODS * DIAGNOSTICS),
    'CONFIGURATION AND ENVIRONMENT',
@@ -44,8 +62,23 @@ our @sections =
    'AUTHOR', 'COPYRIGHT AND LICENSE', 'DISCLAIMER OF WARRANTY');
 
 has qw(+sections default) => sub { \@sections };
-
 #---------------------------------------------------------------------
+
+=attr abstract
+
+The abstract for the module.  Required by NAME.
+
+=attr module
+
+The name of the module.
+Required by NAME and CONFIGURATION AND ENVIRONMENT.
+
+=method section_NAME
+
+  <module> - <abstract>
+
+=cut
+
 has qw(abstract is ro  isa Str);
 has qw(module   is ro  isa Str);
 
@@ -57,8 +90,29 @@ sub section_NAME
 
   "=head1 $title\n\n$module - $abstract\n";
 } # end section_NAME
-
 #---------------------------------------------------------------------
+
+=attr version
+
+The version number of the module.  Used by VERSION.
+
+=attr version_desc
+
+The complete text of the VERSION section. Used by VERSION.
+
+=method section_VERSION
+
+  <version_desc>
+
+Or, if L</"version_desc"> is not set:
+
+  version <version>
+
+If neither version_desc nor L</"version"> is set, no VERSION section
+will be added.
+
+=cut
+
 has qw(version      is ro  isa Str);
 has qw(version_desc is ro  isa Str);
 
@@ -76,16 +130,30 @@ sub section_VERSION
 
   '';                           # Otherwise, omit VERSION
 } # end section_VERSION
-
 #---------------------------------------------------------------------
+
+=method section_ATTRIBUTES
+
+If the original document contains any C<=attr> commands, they will be
+converted to C<=head2> commands in this section.  If there are no
+attributes, no ATTRIBUTES section will be added.
+
+=cut
+
 sub section_ATTRIBUTES
 {
   my $self = shift;
 
   $self->joined_section(attr => @_);
 } # end section_ATTRIBUTES
-
 #---------------------------------------------------------------------
+
+=method section_METHODS
+
+This is just like ATTRIBUTES, except it gathers C<=method> entries.
+
+=cut
+
 sub section_METHODS
 {
   my $self = shift;
@@ -121,8 +189,16 @@ sub joined_section
 
   return $pod;
 } # end joined_section
-
 #---------------------------------------------------------------------
+
+=method section_DIAGNOSTICS
+
+If the original document contains any C<=diag> commands, they will be
+converted to an C<=item> list in this section.  If there are no
+diagnostics, no DIAGNOSTICS section will be added.
+
+=cut
+
 sub section_DIAGNOSTICS
 {
   my ($self, $title, $pod) = @_;
@@ -142,8 +218,14 @@ sub section_DIAGNOSTICS
 
   return $pod . "\n=back\n";
 } # end joined_section
-
 #---------------------------------------------------------------------
+
+=method section_CONFIGURATION_AND_ENVIRONMENT
+
+ <module> requires no configuration files or environment variables.
+
+=cut
+
 sub section_CONFIGURATION_AND_ENVIRONMENT
 {
   my ($self, $title) = @_;
@@ -156,24 +238,59 @@ sub section_CONFIGURATION_AND_ENVIRONMENT
 $module requires no configuration files or environment variables.
 END CONFIGURATION
 } # end section_CONFIGURATION_AND_ENVIRONMENT
-
 #---------------------------------------------------------------------
+
+=method section_INCOMPATIBILITIES
+
+ None reported.
+
+=cut
+
 sub section_INCOMPATIBILITIES
 {
   my ($self, $title) = @_;
 
   "=head1 $title\n\nNone reported.\n";
 } # end section_INCOMPATIBILITIES
-
 #---------------------------------------------------------------------
+
+=method section_BUGS_AND_LIMITATIONS
+
+ No bugs have been reported.
+
+=cut
+
 sub section_BUGS_AND_LIMITATIONS
 {
   my ($self, $title) = @_;
 
   "=head1 $title\n\nNo bugs have been reported.\n";
 } # end section_BUGS_AND_LIMITATIONS
-
 #---------------------------------------------------------------------
+
+=attr dist
+
+The name of the distribution that contains this module.
+Required by AUTHOR.
+
+=attr authors
+
+An arrayref of author names (with optional email address in C<< <> >>).
+Required by AUTHOR.
+
+=method section_AUTHOR
+
+First, it lists the authors from the L</"authors"> attribute
+(converting @ to AT in email addresses).  Then it directs bug reports
+to the distribution's queue at rt.cpan.org (using the L</"dist"> attribute):
+
+  Please report any bugs or feature requests to
+  S<< C<< <bug-<dist> AT rt.cpan.org> >> >>,
+  or through the web interface at
+  L<http://rt.cpan.org/Public/Bug/Report.html?Queue=<dist>>
+
+=cut
+
 has qw(dist    is ro  isa Str);
 has qw(authors is ro  isa ArrayRef[Str]);
 
@@ -203,8 +320,19 @@ or through the web interface at
 L<http://rt.cpan.org/Public/Bug/Report.html?Queue=$dist>
 END AUTHOR
 } # end section_AUTHOR
-
 #---------------------------------------------------------------------
+
+=attr license_notice
+
+The complete text of the COPYRIGHT AND LICENSE section.
+Required by COPYRIGHT AND LICENSE.
+
+=method section_COPYRIGHT_AND_LICENSE
+
+  <license_notice>
+
+=cut
+
 has qw(license_notice is ro  isa Str);
 
 sub section_COPYRIGHT_AND_LICENSE
@@ -216,8 +344,14 @@ sub section_COPYRIGHT_AND_LICENSE
   #FIXME other license
   "=head1 $title\n\n$notice";
 } # end section_COPYRIGHT_AND_LICENSE
-
 #---------------------------------------------------------------------
+
+=method section_DISCLAIMER_OF_WARRANTY
+
+See L</"DISCLAIMER OF WARRANTY">.
+
+=cut
+
 sub section_DISCLAIMER_OF_WARRANTY
 {
   my ($self, $title) = @_;
@@ -254,3 +388,48 @@ END DISCLAIMER
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
+
+__END__
+
+=for Pod::Loom-sort_method
+section_NAME
+section_VERSION
+section_ATTRIBUTES
+section_METHODS
+section_DIAGNOSTICS
+section_CONFIGURATION_AND_ENVIRONMENT
+section_INCOMPATIBILITIES
+section_BUGS_AND_LIMITATIONS
+section_AUTHOR
+section_COPYRIGHT_AND_LICENSE
+section_DISCLAIMER_OF_WARRANTY
+
+=head1 DESCRIPTION
+
+Pod::Loom::Template::Default is the default template for Pod::Loom.
+It places the sections in this order:
+
+  +  NAME
+  +  VERSION
+     SYNOPSIS
+     DESCRIPTION
+  ++ ATTRIBUTES
+  ++ METHODS
+     *
+  ++ DIAGNOSTICS
+  +  CONFIGURATION AND ENVIRONMENT
+     DEPENDENCIES
+  +  INCOMPATIBILITIES
+  +  BUGS AND LIMITATIONS
+  +  AUTHOR
+  +  COPYRIGHT AND LICENSE
+  +  DISCLAIMER OF WARRANTY
+
+Sections marked with C<+> will be provided by this template if they do
+not appear in the original document.  Sections marked C<++> will be
+appended to even if they do appear in the original document (if the
+document contains any entries that belong in that section).
+
+See L<Pod::Loom::Template/"Controlling the template"> for details on
+how to rearrange sections and sort entries.
+
